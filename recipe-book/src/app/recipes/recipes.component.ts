943b2359f35bc343830fc617c85806a2c31a9fdf
Yash <yashpatel256@gmail.com>
2016-10-08 01:26:03 -0400

    [feat-final]

--- recipe-book/src/app/recipes/recipes.component.ts
+++ recipe-book/src/app/recipes/recipes.component.ts
@@ -4,8 +4,7 @@ import { RecipeService } from "./recipe.service";
 
 @Component({
   selector: 'rb-recipes',
-  templateUrl: './recipes.component.html',
-  providers: [ RecipeService ]
+  templateUrl: './recipes.component.html'
 })
 export class RecipesComponent {
 }
